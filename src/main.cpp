27fb6c47d053c4e578d4c16c482a3591cf6d4c9b
Colin Heffernan <colinpheffernan@gmail.com>
2023-11-28 22:54:56 -0500

    Fix rendering and live-solving

--- src/main.cpp
+++ src/main.cpp
@@ -106,15 +106,69 @@ vector<tuple<int, int, vector<int>>> mergeSort(vector<tuple<int, int, vector<int
     return merge(left, right);
 }
 
+void drawSudokuBoard(SudokuBoard board, sf::RenderWindow& window, int algo) {
+    sf::Font font;
+
+    if (!font.loadFromFile("assets/arial.ttf")) {
+        std::cerr << "Error loading font." << std::endl;
+        return;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(36); //font size
+    text.setFillColor(sf::Color::Black);
+
+    float cellSize = 48.0f;
+    float borderSize = 8.0f;
+
+    sf::RectangleShape background(sf::Vector2f((10 * borderSize) + (9 * cellSize), (10 * borderSize) + (9 * cellSize)));
+    background.setOrigin(sf::Vector2f(((10 * borderSize) + (9 * cellSize)) / 2, ((10 * borderSize) + (9 * cellSize)) / 2));
+    background.setFillColor(sf::Color::Black);
+    background.setPosition(640, 320);
+
+    sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+    cell.setOrigin(sf::Vector2f(cellSize / 2, cellSize / 2));
+    cell.setFillColor(sf::Color::White);
+
+    window.clear(sf::Color::White);
+    window.draw(background);
+    float initPosX = 640 - ((cellSize + borderSize) * 4);
+    float initPosY = 320 - ((cellSize + borderSize) * 4);
+    float posX = initPosX;
+    float posY = initPosY;
+    for(unsigned int i = 0; i < 9; i++) {
+        for(unsigned int j = 0; j < 9; j++) {
+            cell.setPosition(posX, posY);
+            window.draw(cell);
+            if(board.getValAtCoords(i, j) != 0) {
+                text.setString(to_string(board.getValAtCoords(i, j)));
+            } else {
+                text.setString("");
+            }
+            sf::FloatRect textRect = text.getLocalBounds();
+            text.setOrigin(textRect.left + (textRect.width / 2.0f), textRect.top + (textRect.height / 2.0f));
+            text.setPosition(posX, posY);
+            window.draw(text);
+            posX += cellSize + borderSize;
+        }
+        posX = initPosX;
+        posY += cellSize + borderSize;
+    }
+    window.display();
+}
+//clone of other board function to test multiple boards
+
 // Solves the board using Backtracking
-bool solveBacktracking(SudokuBoard& board) {
+bool solveBacktracking(SudokuBoard& board, sf::RenderWindow& window) {
     vector<int> coords = board.findNext();
     if(coords.empty()) return true;
     for(unsigned int i = 1; i <= 9; i++) {
         if(board.isSafe(i, coords[0], coords[1])) {
             board.place(i, coords[0], coords[1]);
             // PLACE DRAW FUNC HERE
-            if(solveBacktracking(board)) return true;
+            drawSudokuBoard(board, window, 0);
+            if(solveBacktracking(board, window)) return true;
             board.place(0, coords[0], coords[1]);
         }
     }
@@ -122,7 +176,7 @@ bool solveBacktracking(SudokuBoard& board) {
 }
 
 // Solves the board using Cross-Hatching
-bool solveCrossHatch(SudokuBoard& board) {
+bool solveCrossHatch(SudokuBoard& board, sf::RenderWindow& window) {
     vector<pair<int, int>> mutables = board.findMutables();
     vector<tuple<int, int, vector<int>>> options;
     for(pair<int, int> c : mutables) {
@@ -138,60 +192,12 @@ bool solveCrossHatch(SudokuBoard& board) {
     for(int i : get<2>(options[0])) {
         board.place(i, get<0>(options[0]), get<1>(options[0]));
         // PLACE DRAW FUNC HERE
-        if(solveCrossHatch(board)) return true;
+        drawSudokuBoard(board, window, 1);
+        if(solveCrossHatch(board, window)) return true;
         board.place(0, get<0>(options[0]), get<1>(options[0]));
     }
     return false;
 }
-
-void drawSudokuBoard(sf::RenderWindow& window, const std::vector<std::vector<char>>& board, int currentRow, int currentCol) {
-    sf::Font font;
-
-    if (!font.loadFromFile("../../assets/arial.ttf")) {
-        std::cerr << "Error loading font." << std::endl;
-        return;
-    }
-
-    sf::Text text;
-    text.setFont(font);
-    text.setCharacterSize(40); //font size
-    text.setFillColor(sf::Color::Black);
-
-    float cellSize = 60.0f;
-    float boldLineThickness = 9.0f; // thickness of the bold lines
-
-    sf::RectangleShape boldLine(sf::Vector2f(cellSize * 3, boldLineThickness));
-    boldLine.setFillColor(sf::Color::Black);
-
-    for (size_t i = 0; i < 9; ++i) {
-        for (size_t j = 0; j < 9; ++j) {
-
-            // draw cell
-            sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
-            cell.setFillColor(sf::Color::White);
-            cell.setOutlineThickness(3.0f);
-            cell.setOutlineColor(sf::Color::Black);
-            cell.setPosition(static_cast<float>(j) * cellSize, static_cast<float>(i) * cellSize);
-            window.draw(cell);
-
-            // draw number
-            text.setString(board[i][j]);
-            if (i < currentRow || (i == currentRow && j <= currentCol)) {
-                // draw solved numbers in black
-                text.setFillColor(sf::Color::Black);
-            } else {
-                // draw current solving numbers in red
-                text.setFillColor(sf::Color::Red);
-            }
-
-            sf::FloatRect textRect = text.getLocalBounds();
-            text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-            text.setPosition(static_cast<float>(j) * cellSize + cellSize / 2.0f, static_cast<float>(i) * cellSize + cellSize / 2.0f);
-            window.draw(text);
-        }
-    }
-}
-//clone of other board function to test multiple boards
 
 // // Solve the Sudoku using backtracking
 // bool solveSudoku(sf::RenderWindow& window, vector<vector<char>>& board, int row, int col) {
@@ -225,128 +231,75 @@ void drawSudokuBoard(sf::RenderWindow& window, const std::vector<std::vector<cha
 // }
 
 int main() {
-    SudokuBoards sudokuBoards; // create an instance of SudokuBoards to store multiple boards
+    vector<SudokuBoard> boards = initializeBoards();
+    map<string, sf::Texture> textures = initializeTextures();
 
-    ifstream inputFile("../sudokuNew.csv");
-
-    if (!inputFile.is_open()) {
-        cerr << "Error opening the file." << endl;
-        return 1;
-    }
-
-    string line;
-    while (getline(inputFile, line)) {
-        size_t commaPos = line.find(',');
-        if (commaPos != string::npos) {
-            string unsolved = line.substr(0, commaPos);
-            string solved = line.substr(commaPos + 1);
-
-            SudokuBoards::SudokuBoard sudokuBoard(unsolved, solved);
-            sudokuBoards.sudokuBoards.push_back(sudokuBoard);
-
-            //cout << "Unsolved:" << std::endl;
-            //printBoard(sudokuBoard.unsolved);
-
-            //cout << "Solved:" << std::endl;
-            //printBoard(sudokuBoard.solved);
-        } else {
-            std::cerr << "Invalid line format in CSV file." << std::endl;
-        }
-    }
-
-
-    sf::RenderWindow welcome_window(sf::VideoMode(800, 800), "Welcome");
+    sf::RenderWindow welcome_window(sf::VideoMode(1280, 720), "Speedoku");
     welcome_window.clear(sf::Color::White);
 
-    TextureManager manager; //texture manager object
-
-    sf::Sprite icon;
-    icon.setTexture(*manager.texture_map["icon"]);
-    auto texture_temp = *manager.texture_map["icon"];
-    icon.setOrigin(sf::Vector2f(texture_temp.getSize().x / 2, texture_temp.getSize().y / 2));
-    icon.setScale(0.65f, 0.65f);
-    icon.setPosition(400, 375);
-
-
+    sf::Sprite sudoku_icon;
+    sudoku_icon.setTexture(textures["sudoku_icon"]);
+    sudoku_icon.setOrigin(sf::Vector2f(textures["sudoku_icon"].getSize().x / 2, textures["sudoku_icon"].getSize().y / 2));
+    sudoku_icon.setScale(1.0f, 1.0f);
+    sudoku_icon.setPosition(640, 200);
 
     sf::Sprite speedoku_text;
-    speedoku_text.setTexture(*manager.texture_map["speedoku_text"]);
-    texture_temp = *manager.texture_map["speedoku_text"];
-    speedoku_text.setOrigin(sf::Vector2f(texture_temp.getSize().x / 2, texture_temp.getSize().y / 2));
-    speedoku_text.setScale(1.25f, 1.25f);
-    speedoku_text.setPosition(400, 125);
-
+    speedoku_text.setTexture(textures["speedoku_text"]);
+    speedoku_text.setOrigin(sf::Vector2f(textures["speedoku_text"].getSize().x / 2, textures["speedoku_text"].getSize().y / 2));
+    speedoku_text.setScale(1.0f, 1.0f);
+    speedoku_text.setPosition(640, 120);
 
     sf::Sprite start_icon;
-    start_icon.setTexture(*manager.texture_map["start_icon"]);
-    texture_temp = *manager.texture_map["start_icon"];
-    start_icon.setOrigin(sf::Vector2f(texture_temp.getSize().x / 2, texture_temp.getSize().y / 2));
+    start_icon.setTexture(textures["start_icon"]);
+    start_icon.setOrigin(sf::Vector2f(textures["start_icon"].getSize().x / 2, textures["start_icon"].getSize().y / 2));
     start_icon.setScale(0.65f, 0.65f);
-    start_icon.setPosition(400, 675);
+    start_icon.setPosition(640, 480);
 
-
-
+    int mode = 0;
+    int board = 0;
     while(welcome_window.isOpen()){ //welcome window loop
-
         sf::Event event;
         while (welcome_window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 welcome_window.close();
-            }
-        }
-        if(event.type == sf::Event::MouseButtonPressed){
-            sf::Mouse mouse;
-            bool left;
-            auto coordinate = mouse.getPosition(welcome_window);
-
-            int x_size = start_icon.getTexture()->getSize().x * start_icon.getScale().x;
-            int y_size = start_icon.getTexture()->getSize().y * start_icon.getScale().y;
-            int y_position = start_icon.getPosition().y;
-            if(coordinate.x >= 400 - (x_size / 2) and coordinate.x <= 400 + (x_size / 2)){ //if start button pressed
-                if(coordinate.y >= y_position - (y_size / 4) and coordinate.y <= y_position + (y_size / 4)){
-                    welcome_window.close();
+            } else {
+                if(mode == 0) {
+                    if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Button::Left){
+                        int x_size = start_icon.getTexture()->getSize().x * start_icon.getScale().x;
+                        int y_size = start_icon.getTexture()->getSize().y * start_icon.getScale().y;
+                        int x_position = start_icon.getPosition().x;
+                        int y_position = start_icon.getPosition().y;
+                        if(event.mouseButton.x >= x_position - (x_size / 2) && event.mouseButton.x <= x_position + (x_size / 2) && event.mouseButton.y >= y_position - (y_size / 2) && event.mouseButton.y <= y_position + (y_size / 2)) {
+                            mode = 1;
+                        }
+                    }
                 }
             }
         }
 
-        welcome_window.clear(sf::Color::White);
-        welcome_window.draw(icon);
-        welcome_window.draw(start_icon);
-        welcome_window.draw(speedoku_text);
-        welcome_window.display();
-    }
-
-    sf::RenderWindow window(sf::VideoMode(1080, 1080), "Sudoku Board");
-    window.clear(sf::Color::White);
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        window.clear(); //keeps board from flashing
-        if (!sudokuBoards.sudokuBoards.empty()) {
-            int boardIndex = 1;
-            vector<vector<char>> unsolvedBoard = sudokuBoards.sudokuBoards[boardIndex].unsolved;
-
-            // print the unsolved board for debugging
-            cout << "Unsolved Board:" << endl;
-            printBoard(unsolvedBoard);
-
-            vector<vector<char>> solvedBoard = unsolvedBoard;  // create a copy of the unsolved board
-
-            for (int row = 0; row < 9; ++row) {
-                for (int col = 0; col < 9; ++col) {
-                    solveSudoku(window, solvedBoard, col, row);
-                    drawSudokuBoard(window, solvedBoard, row, col);
-                    window.display();
-                    sf::sleep(sf::milliseconds(100)); // adjust the delay as neede
-                }
-            }
+        if(mode == 0) {
+            welcome_window.clear(sf::Color::White);
+            welcome_window.draw(sudoku_icon);
+            welcome_window.draw(start_icon);
+            welcome_window.draw(speedoku_text);
+            welcome_window.display();
+        } else if(mode == 1) {
+            // TODO: Implement solve screens.
+            drawSudokuBoard(boards[board], welcome_window, 0);
+            sf::sleep(sf::milliseconds(2500));
+            SudokuBoard boardAlgo1 = boards[board];
+            solveBacktracking(boardAlgo1, welcome_window);
+            sf::sleep(sf::milliseconds(2500));
+            drawSudokuBoard(boards[board], welcome_window, 0);
+            sf::sleep(sf::milliseconds(2500));
+            SudokuBoard boardAlgo2 = boards[board];
+            solveCrossHatch(boardAlgo2, welcome_window);
+            welcome_window.close();
+        } else if(mode == 2) {
+            // TODO: Implement post-solve time comparison screen.
         }
     }
+    
     return 0;
 }
-*/
 
